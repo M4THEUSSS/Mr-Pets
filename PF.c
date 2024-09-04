c410d8dfc7a88fea7161faff1225aa9a5456d742
M4THEUSSS <m.t.s_@hotmail.com>
2024-09-04 14:51:45 -0300

    Fim da Atividade
    version 2.0

--- PF.c
+++ PF.c
@@ -1,10 +1,10 @@
-namespace Name
+namespace Mr-Pets
 {
     
-}
+    }
 
-class Pessoa_Fisica : Clientes
-{
-public string cpf {get; set;}
-public string rg {get; set;}
+         class Pessoa_Fisica : Clientes
+    {
+         public string cpf {get; set;}
+         public string rg {get; set;}
 }
